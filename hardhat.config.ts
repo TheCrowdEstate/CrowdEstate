8edeb39b86851b90823096d711a6be9bff0411bf
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2023-05-27 10:41:35 +0100

    chore: update package and setup different networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox'
+import '@openzeppelin/hardhat-upgrades'
+import * as dotenv from 'dotenv'
+import { HardhatUserConfig } from 'hardhat/config'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-};
+  solidity: '0.8.18',
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_RPC_URL || '',
+        enabled: true,
+      },
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
+  },
+}
 
-export default config;
+export default config
